2661f01c1d9a9ae51f0647dac0b537de644a6b08
Dominic <otienodominic@gmail.com>
2021-03-07 11:54:24 +0300

    resolved the props bug

--- react-ui/src/components/pages/Resource.js
+++ react-ui/src/components/pages/Resource.js
@@ -2,44 +2,58 @@ import React, { useEffect, useState, useMemo } from 'react'
 import { Container } from 'reactstrap';
 import TableContainer from '../Facility/TableContainer';
 
-function Resource(props) {
-console.log(props)
+function Resource(props) {  
+const [data, setData] = useState([]);
 
+useEffect(() => {
+  const doFetch = async () => {
+    let url = `https://play.dhis2.org/2.34.3/api/${props.location.plural}`
+            let username = 'admin';
+            let password = 'district';
+            let headers = new Headers();
 
-// console.log(data)
-//   const columns = useMemo(
-//     () => [
-//       {
-//         Header: () => null,
-//         id: 'expander', // 'id' is required
-//         Cell: ({ row }) => (
-//           <span {...row.getToggleRowExpandedProps()}>
-//             {row.isExpanded ? '👇' : '👉'}
-//           </span>
-//         ),
-//       },      
-//       {
-//         Header: 'ID',
-//         accessor: 'id',
-//       },
-//       {
-//         Header: 'Resource Name',
-//         accessor: 'displayName',
-//       },
+            headers.set('Authorization', 'Basic ' + btoa(username + ":" + password));
+
+            const response = await fetch(url, {method:'GET',
+                    headers: headers,               
+                })                
+            const body = await response.json()  
+            
+            // destructure the required object here
+            const allowed = [props.location.plural]
+            Object.keys(body)
+                .filter(key => !allowed.includes(key))
+                .forEach(key => delete body[key]); 
+            // get the object 
+        const mural = Object.values(body)[0]
+        setData(mural)
+  };
+  doFetch();
+}, [props]);
+
+  const columns = useMemo(
+    () => [
       
-//     ],
-//     []
-//   );
+      {
+        Header: 'ID',
+        accessor: 'id',
+      },
+      {
+        Header: 'Resource Name',
+        accessor: 'displayName',
+      },
+      
+    ],
+    []
+  );
 
   return (
-    // <Container style={{ marginTop: 100 }}>
-    //   <TableContainer
-    //     columns={columns}
-    //     data={data}       
-    //   />
-    // </Container>
-
-    <div>hello</div>
+    <Container style={{ marginTop: 100 }}>
+      <TableContainer
+        columns={columns}
+        data={data}       
+      />
+    </Container>    
   );
 }
 
